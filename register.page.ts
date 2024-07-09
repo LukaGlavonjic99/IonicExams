00a2af80f0963743004fc8e968c917bf3c49afc3
Luka <144832285+LukaGlavonjic99@users.noreply.github.com>
2024-07-09 23:06:05 +0200

    Create register.page.ts

---
+++ register.page.ts
@@ -0,0 +1,51 @@
+import { Component} from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
+import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage  {
+
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toastController: ToastController
+  ) { }
+
+  async register() {
+    if (!this.email || !this.password || !this.confirmPassword) {
+      this.showErrorToast('Please fill out all fields.');
+      return;
+    }
+
+    if (this.password !== this.confirmPassword) {
+      this.showErrorToast('Passwords do not match.');
+      return;
+    }
+
+    try {
+      await this.authService.register(this.email, this.password);
+      this.router.navigateByUrl('/home', { replaceUrl: true });
+    } catch (error) {
+      this.showErrorToast('Registration failed. Please try again.');
+    }
+  }
+  async showErrorToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 4000,
+      color: 'danger',
+      position: 'top'
+    });
+    toast.present();
+  }
+
+
+}
